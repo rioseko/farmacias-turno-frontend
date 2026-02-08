a2394b51db4b9167b6d20fc8971bc5a40dcf31df
Juan Rioseco <juan.riosecozelada@gmail.com>
2026-02-07 21:23:35 -0300

    fix-prod-url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  // IMPORTANTE: Reemplaza esta URL con la dirección real de tu backend desplegado en Netlify.
-  // Si tu sitio en Netlify se llama distinto a 'farmacias-turno-backend', ajusta la parte inicial.
-  apiUrl: 'https://farmacias-turno-backend.netlify.app/.netlify/functions/api/farmacias'
+  apiUrl: 'https://midas.minsal.cl/farmacia_v2/WS/getLocalesTurnos.php'
 };
