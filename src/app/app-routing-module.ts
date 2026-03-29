be9fa3c8f5b1b4a50eae60eb0ad6d31cad4e60d0
Juan Rioseco <juan.riosecozelada@gmail.com>
2026-03-28 21:02:14 -0300

    add redirection from / to /farmacia-de-turno-temuco

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router'
 import { FarmaciasPageComponent } from './pages/farmacias-page/farmacias-page.component'
 
 const routes: Routes = [
-  { path: '', component: FarmaciasPageComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'farmacia-de-turno-temuco' },
   { path: 'farmacia-de-turno-temuco', component: FarmaciasPageComponent },
 ]
 
